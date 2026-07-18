d1e4d5dd52beda53f1d52fb5af29f36a600a26c4
mg-devop <gopidevops2025@gmail.com>
2026-07-18 17:27:14 +0530

    Update profile.utils.ts

--- src/app/routes/profile/profile.utils.ts
+++ src/app/routes/profile/profile.utils.ts
@@ -1,13 +1,14 @@
 import { User } from '../auth/user.model';
 import { Profile } from './profile.model';
 
-const profileMapper = (user: any, id: number | undefined): Profile => ({
-  username: user.username,
-  bio: user.bio,
-  image: user.image,
-  following: id
-    ? user?.followedBy.some((followingUser: Partial<User>) => followingUser.id === id)
-    : false,
-});
+const profileMapper = (user: any, id?: number) => {
+  return {
+    username: user.username,
+    bio: user.bio,
+    // Add this fallback logic:
+    image: user.image || 'https://static.productionready.io/images/smiley-cyrus.jpg',
+    following: id ? user.followedBy.some((follow: any) => follow.id === id) : false,
+  };
+};
 
 export default profileMapper;
