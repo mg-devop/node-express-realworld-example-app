e96cb9e37f19f97e157e363d4e5a2c3dadc7f025
mg-devop <gopidevops2025@gmail.com>
2026-07-19 14:22:31 +0530

    Update profile.utils.ts

--- src/app/routes/profile/profile.utils.ts
+++ src/app/routes/profile/profile.utils.ts
@@ -6,7 +6,7 @@ const profileMapper = (user: any, id?: number) => {
     username: user.username,
     bio: user.bio,
     // Add this fallback logic:
-    image: user.image || `https://ui-avatars.com/api/?name=${user.username}&size=128&background=random`,
+    image: user.image || '/assets/images/smiley-cyrus.jpeg',
     following: id ? user.followedBy.some((follow: any) => follow.id === id) : false,
   };
 };
